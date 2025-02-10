5947340dfb93753dc164a8b9b312c0f5b49d7552
Sergey Makarov <sergejmakarov30043@gmail.com>
2025-02-10 20:20:05 +0300

    deploy

--- backend/backend_foodgram/settings.py
+++ backend/backend_foodgram/settings.py
@@ -115,7 +115,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination'
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination',
+    'PAGE_SIZE': 6,
 }
 
 DJOSER = {
